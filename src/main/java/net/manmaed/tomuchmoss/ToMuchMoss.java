86623ecc82d20cb16b678c017ca01e4f8eb58c39
manmaed <manmaed18@gmail.com>
2022-06-26 20:49:24 +0100

    Tags + Loot tables + Recipes

--- src/main/java/net/manmaed/tomuchmoss/ToMuchMoss.java
+++ src/main/java/net/manmaed/tomuchmoss/ToMuchMoss.java
@@ -8,6 +8,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by manmaed on 23/06/2022.
@@ -16,7 +18,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(ToMuchMoss.MOD_ID)
 public class ToMuchMoss {
 
+
     public static final String MOD_ID = "tomuchmoss";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final CreativeModeTab TAB = new CreativeModeTab(MOD_ID) {
         @Override
         public ItemStack makeIcon() {
